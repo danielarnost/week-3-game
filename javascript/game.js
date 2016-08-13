84ede167a3589e0462502c4dfd7a74b32cf886c6
danielarnost <danielarnost@utexaas.edu>
2016-08-13 08:34:54 -0500

    Psychic game style

--- javascript/game.js
+++ javascript/game.js
@@ -1,14 +1,65 @@
 var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         var winsCount = 0,
-            lossesCount = 0,
-            guessesLeftCount = 10,
-            guessesSoFar = ['0' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10']; // array []
+            lossCount = 0,
+            guessesLeft = 10,
+            guessesSoFar = [], // list of letters guessed so far
+            Win = false;
+            computerGuess = Mathfloor(Math.random)()*options.length;
 
-            document.onkeyup = function(event) {
+           document.onkeyup = function(event) {
 
-            // Determines which exact key was selected. Make it lowercase
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+           var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-            
+           
 
-            
+            for (var i = guessesLeftCount.length -1; 1>0;  i--);
+
+
+           //*** alert("Your choice is " + userGuess); HOW DO I GET THIS TO APPEAR ON SCREEN?!
+            var computerGuess = Mathfloor(Math.random)()*options.length;
+
+              if (userGuess == computerGuess) {
+              	document.getElementById()winsCount ++
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else if userGuess !== computerGuess
+              	guessesSoFar++;
+              	guessesLeft--
+              } else {
+
+                alert("You lose");
+                lossCount++;
+            }
+
+
+
+
+
+
+
+
+            }
+
+             
+
