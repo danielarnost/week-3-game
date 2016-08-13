14b6bca457dc884b4e420ec51b9a169a511e5197
danielarnost <danielarnost@utexaas.edu>
2016-08-13 18:59:56 -0500

    some style

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,4 @@
-var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+		var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         var wins = 0,
             losses = 0,
             guessesLeft = 9,
@@ -16,17 +16,24 @@ var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 
            document.onkeyup = function(event) {
            	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	
+			//can we get it so that the user can't choose the same character twice?
+			//alert: That lett ...
            	if (userGuess == computerGuess) {
               	var wins = document.getElementById("wins"); 
               	wins.innerHTML = 1 + parseInt(wins.innerHTML);
               	reset()
+              } else if (guessedSoFar.includes(userGuess)) {
+              alert ("You already guessed that lettter!, choose a different one!");
+              s
             } else if (guessesLeft == 0) { var losses = document.getElementById("losses"); 
             	losses.innerHTML = 1 + parseInt(losses.innerHTML);
                 reset()
             }
-            	guessesLeft -- 
-            	guessedSoFar.push(userGuess)
+            	if (!guessedSoFar.includes(userGuess)) {
+            	guessesLeft--; 
+            	guessedSoFar.push(userGuess) }
+
+            	
             	var guessed = document.getElementById("guessed");
             	guessed.innerHTML = guessedSoFar
 
